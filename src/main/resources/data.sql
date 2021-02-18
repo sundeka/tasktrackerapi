5c88b6b930464a4cb95394fc9090ad09645be4fa
Kalle Sundell <sundellkalle@gmail.com>
2021-02-18 13:26:13 +0200

    Valuen lisäys SQL-tiedostoon + git push test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into task values(0, '28', 'Helmi', false, 'Pese pyykit.', '2021');
 insert into task values(1, '13', 'Huhti', true, 'Mene töihin.', '2022');
+insert into task values(2, '8', 'Touko', true, 'Haistattelu.', '2023');
